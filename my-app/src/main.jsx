821e01ff768963c9d707001bfe13e0217bc1ad17
Riyaa-k <anshitakoshta28@gmail.com>
2023-11-23 23:24:07 +0530

    change

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import Message from './message.jsx'
+import Counter from './components/Counter.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    {/* <Message /> */}
+    <Counter />
+
   </React.StrictMode>,
 )
